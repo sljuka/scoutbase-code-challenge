f7cc31ef62e6ef97c41f9b9b4856835afadebe9b
David Sljukic <david.sljukic.dev@gmail.com>
2019-08-30 14:26:01 +0200

    Integrate express with apollo-server; add simple GQL schema

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+
 const app = express();
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!"
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: "/graphql" });
 
 app.get("/", function(req, res) {
   res.send("Hello World");
