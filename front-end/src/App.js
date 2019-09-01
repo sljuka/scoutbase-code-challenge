b6699036e88cbdcf8cde5e5be58a33264ea76bc5
David Sljukic <david.sljukic.dev@gmail.com>
2019-09-01 16:13:55 +0200

    Add index, countries and country routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,39 @@
 import React from "react";
-
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { Countries } from "./Countries";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "https://countries.trevorblades.com"
 });
 
+const Index = () => <p>Welcome to the countries app</p>;
+const Country = () => <p>Country</p>;
+
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div>
-        <h2>My first Apollo app 🚀</h2>
-        <Countries />
-      </div>
+      <Router>
+        <div>
+          <h2>Countries app 🌎</h2>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/countries">Countries</Link>
+            </li>
+            <li>
+              <Link to="/country/CI">Country</Link>
+            </li>
+          </ul>
+        </div>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/countries/" exact component={Countries} />
+        <Route path="/country/:code" component={Country} />
+      </Router>
     </ApolloProvider>
   );
 }
